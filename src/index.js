570a0ddef7dcd5101615e3b543883111b11b9c95
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-03-09 17:35:31 +0200

    add fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import debounce from 'lodash/debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const searchEl = document.querySelector('#search-box')
+searchEl.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
+
+searchEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
