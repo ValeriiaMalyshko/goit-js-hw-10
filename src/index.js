e455212c840841847b9933aeac8ca39814e3dd08
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-03-09 20:10:22 +0200

    change fetch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import './css/styles.css';
 import debounce from 'lodash/debounce';
 import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
-const searchEl = document.querySelector('#search-box')
+const searchEl = document.querySelector('#search-box');
+const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
+
 searchEl.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
-searchEl.addEventListener('submit', (event) => {
+function handleInput (event) {
     event.preventDefault();
-});
+    const inputValue = event.target.value.trim().toLowerCase();
+     if (inputValue==="") {
+        countryInfo = "";
+        countryList = "";
+     }
+     fetchCountries(`${inputValue}`)
+        .then(renderMarkup)
+        .catch(onFetchError)
+        .finally(() => form.reset());
+};
 
+function onFetchError(error) {
+    countryListRef.innerHTML = '';
+    countryInfoRef.innerHTML = '';
+    Notify.failure('Oops, there is no country with that name.');
+  }
